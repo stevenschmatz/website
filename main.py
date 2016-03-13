2cca8b45b771c726d1e0e8672e9bd13f34eeb93a
Steven Schmatz <stevenschmatz@gmail.com>
2016-03-13 18:20:49 -0400

    added config file

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import os
+from config import config as Config
 from flask import Flask, render_template, request, redirect
-import pymongo
 from pymongo import MongoClient
 
+# Open up MongoDB connection
 MONGO_URL = os.environ.get('MONGOLAB_URI')
+if MONGO_URL is None:
+    MONGO_URL = Config.debug_mongo_uri
+
 client = MongoClient(MONGO_URL)
 
 # Specify the database
@@ -12,19 +16,22 @@ collection = db.shoutouts
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=['GET'])
 def index():
     shouts = collection.find()
     return render_template('index.html', shouts=shouts)
 
+
 @app.route("/post", methods=['POST'])
 def post():
-    shout = {"name":request.form['name'], "message":request.form['message']}
-    shout_id = collection.insert(shout)
+    shout = {
+        "name": request.form['name'],
+        "message": request.form['message']
+    }
+    collection.insert(shout)
     return redirect('/')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
-
-# sPI4ehLbQyIeEpbpNajVofWo
+    app.run(host='0.0.0.0', port=port, debug=True)
